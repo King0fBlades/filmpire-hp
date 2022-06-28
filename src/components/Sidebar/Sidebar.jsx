ad205a24aade11a71aed340a6eee04b76749a423
Hristo Petrov <king0fblader98@mail.bg>
2022-06-29 01:46:38 +0300

    movie categories and genre icons

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -12,7 +12,9 @@ import {
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
 
+import { useGetGenresQuery } from '../../services/TMDB';
 import useStyles from './styles';
+import genreIcons from '../../assets/genres';
 
 const categories = [
 	{ label: 'Popular', value: 'popular' },
@@ -35,6 +37,7 @@ const blueLogo =
 const Sidebar = ({ setMobileOpen }) => {
 	const theme = useTheme();
 	const classes = useStyles();
+	const { data, isFetching } = useGetGenresQuery();
 
 	return (
 		<>
@@ -51,13 +54,13 @@ const Sidebar = ({ setMobileOpen }) => {
 				{categories.map(({ label, value }) => (
 					<Link key={value} className={classes.links} to="/">
 						<ListItem onClick={() => {}} button>
-							{/* <ListItemIcon>
+							<ListItemIcon>
 								<img
-									src={redLogo}
+									src={genreIcons[label.toLowerCase()]}
 									className={classes.genreImages}
 									height={30}
 								/>
-							</ListItemIcon> */}
+							</ListItemIcon>
 							<ListItemText primary={label} />
 						</ListItem>
 					</Link>
@@ -66,20 +69,26 @@ const Sidebar = ({ setMobileOpen }) => {
 			<Divider />
 			<List>
 				<ListSubheader>Genres</ListSubheader>
-				{demoCategories.map(({ label, value }) => (
-					<Link key={value} className={classes.links} to="/">
-						<ListItem onClick={() => {}} button>
-							{/* <ListItemIcon>
-								<img
-									src={redLogo}
-									className={classes.genreImages}
-									height={30}
-								/>
-							</ListItemIcon> */}
-							<ListItemText primary={label} />
-						</ListItem>
-					</Link>
-				))}
+				{isFetching ? (
+					<Box display="flex" justifyContent="center">
+						<CircularProgress />
+					</Box>
+				) : (
+					data.genres.map(({ name, id }) => (
+						<Link key={name} className={classes.links} to="/">
+							<ListItem onClick={() => {}} button>
+								<ListItemIcon>
+									<img
+										src={genreIcons[name.toLowerCase()]}
+										className={classes.genreImages}
+										height={30}
+									/>
+								</ListItemIcon>
+								<ListItemText primary={name} />
+							</ListItem>
+						</Link>
+					))
+				)}
 			</List>
 		</>
 	);
